59590869ed6c2bdc538430e759c2984a47d45572
gamal sob <133385330+gamalsobih@users.noreply.github.com>
2024-08-09 00:01:55 +0300

    Update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,23 +1,24 @@
+/* eslint-disable no-underscore-dangle */
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this.floors = floors;
+    this._floors = floors;
   }
 
   get floors() {
     return this._floors;
   }
 
-  set floors(newFloors) {
-    if (typeof newFloors !== 'number') {
-      throw TypeError('Floors must be a number');
+  set floors(val) {
+    if (typeof val !== 'number') {
+      throw new Error();
     }
-    this._floors = newFloors;
+    this._floors = val;
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
